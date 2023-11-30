520b669ec20642a84b81c88f6b084b32848df4e0
Douglas Moreira <dougmoreira@Douglass-MBP.lan>
2023-11-30 15:36:40 -0500

    share btn test2

--- app/components/button.js
+++ app/components/button.js
@@ -6,13 +6,12 @@ export default function Button({currentUser}) {
   const shareData = {
     title: "Mean Girls Musical 2024",
     text: "This event is so fetch! #meangirlsmusical2024",
-    url: `${currentUser.Buttonphoto.original.url}`
+    url: `${currentUser.photo.original.url}`
 
   }
   const share = () =>{
     navigator.share(shareData)
   }
-  
   // const saveFile = () => {
   //   saveAs(
   //   `${ç}`,
