bc182dd479eaa51ca57ef5ae0c528fb6cad0b19d
Douglas Moreira <dougmoreira@Douglass-MBP.lan>
2023-12-01 10:42:16 -0500

    analytics

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,7 @@ import { Inter } from 'next/font/google'
 import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
+import { Analytics } from '@vercel/analytics/react';
 
 export const metadata = {
   title: 'Mean Girls Booth',
@@ -11,7 +12,10 @@ export const metadata = {
 export default async function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {children}
+        <Analytics/>
+      </body>
     </html>
   )
 }
